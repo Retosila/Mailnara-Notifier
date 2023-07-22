7765fca29dbbd0b7bdee8831c6a180507d268f5b
Retosila <darksky1010@naver.com>
2023-07-22 13:56:20 +0900

    Manage version with a centralized way

---
+++ script.version.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const versionInfo = JSON.parse(fs.readFileSync("version.json", "utf8"));
+const version = versionInfo.version;
+
+// Update manifest.json version.
+const manifest = JSON.parse(fs.readFileSync("src/manifest.json", "utf8"));
+manifest.version = version;
+fs.writeFileSync("src/manifest.json", JSON.stringify(manifest, null, 2));
+
+// Update package.json version.
+const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+packageJson.version = version;
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
+
+// Update package-lock.json version.
+const packageLockJson = JSON.parse(
+  fs.readFileSync("package-lock.json", "utf8")
+);
+packageLockJson.version = version;
+fs.writeFileSync("package-lock.json", JSON.stringify(packageLockJson, null, 2));
